9571b983114a6c680ca981eb42bb1f6a3d9fcfdc
Mariano Cortesi <mcortesi@gmail.com>
2019-06-04 12:41:16 -0300

    Update build files

--- dist/precompiled/fd-transfer.js
+++ dist/precompiled/fd-transfer.js
@@ -5,6 +5,33 @@ var BN = utils.BN;
 var error = require('../exceptions.js').ERROR;
 var assert = require('assert');
 
+function OOGResult(gasLimit) {
+  return {
+    return: Buffer.alloc(0),
+    gasUsed: opts.gasLimit,
+    exception: 0, // 0 means VM fail (in this case because of OOG)
+    exceptionError: error.OUT_OF_GAS
+  };
+}
+
+function incrementBalance(stateManager, address, delta, callback) {
+  stateManager.getAccount(address, function (err, account) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    account.balance = new BN(account.balance).add(delta).toBuffer();
+    stateManager.putAccount(address, account, function (err) {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null);
+      }
+    });
+  });
+}
+
 module.exports = function (opts, cb) {
   assert(opts.data);
 
@@ -13,33 +40,13 @@ module.exports = function (opts, cb) {
   // TODO(asa): Pick an appropriate gas amount
   results.gasUsed = new BN(20);
   if (opts.gasLimit.lt(results.gasUsed)) {
-    results.return = Buffer.alloc(0);
-    results.gasUsed = opts.gasLimit;
-    results.exceptionError = error.OUT_OF_GAS;
-    results.exception = 0; // 0 means VM fail (in this case because of OOG)
-    cb(error.OUT_OF_GAS, results);
+    cb(error.OUT_OF_GAS, OOGResult(gasLimit));
     return;
   }
 
-  var from = new BN(opts.data.slice(0, 32));
-  var to = new BN(opts.data.slice(32, 64));
+  var fromAddress = opts.data.slice(0, 32);
+  var toAddress = opts.data.slice(32, 64);
   var value = new BN(opts.data.slice(64, 96));
-  var incrementBalance = function incrementBalance(address, delta, callback) {
-    opts.stateManager.getAccountBalance(address, function (err, balance) {
-      if (err) {
-        callback(err);
-      } else {
-        var newBalance = new BN(balance).add(delta);
-        opts.stateManager.putAccountBalance(address, newBalance, function (err) {
-          if (err) {
-            callback(err);
-          } else {
-            callback(null);
-          }
-        });
-      }
-    });
-  };
 
   var failIfErr = function failIfErr(err) {
     if (err) {
@@ -49,10 +56,10 @@ module.exports = function (opts, cb) {
     }
   };
 
-  incrementBalance(from, value.neg(), function (err) {
+  incrementBalance(opts.stateManager, fromAddress, value.neg(), function (err) {
     failIfErr(err);
     if (!err) {
-      incrementBalance(to, value, function (err) {
+      incrementBalance(opts.stateManager, toAddress, value, function (err) {
         failIfErr(err);
         if (!err) {
           results.return = true;
