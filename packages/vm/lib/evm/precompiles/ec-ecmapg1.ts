d705ea0d9ea5ddcd796a5512c89805fec150548d
lucasege <lucas@celo.org>
2020-06-24 15:31:11 -0700

    Removing assert

--- packages/vm/lib/evm/precompiles/ec-ecmapg1.ts
+++ packages/vm/lib/evm/precompiles/ec-ecmapg1.ts
@@ -7,15 +7,16 @@ export default function(opts: PrecompileInput): ExecResult {
   assert(opts.data)
 
   // TODO(lucas): Pick an appropriate gas amount
-  const gasUsed = new BN(500)
+  const gasUsed = new BN(50000)
   if (opts.gasLimit.lt(gasUsed)) {
     return OOGResult(opts.gasLimit)
   }
+  // assert(false);
 
-  assert.equal(
-    opts.data,
-    "test"
-  )
+  // assert.equal(
+  //   opts.data,
+  //   "test"
+  // )
   return {
     gasUsed,
     returnValue: Buffer.from("33668b53057a37a1681926b05d40441e92cdc4c227673d04f1f370d3a1d6af3096ec25497d8898c67c70b58313a8870035c5a52ed463fa2637e742dd42fc3cf1661018ff06ff21dd1a8992a217497c8f4245476de570bec10908b08302878a0100", "hex")
